f7944b39aa1601a2d502fa7b2c7543c53f97fb19
Eslam Mostafa <cseslam@gmail.com>
2018-04-27 08:04:07 +0200

    add first test case for YAML and JSON parsers

---
+++ parser/json/json_test.go
@@ -0,0 +1,33 @@
+package json
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestDBConfParsing(t *testing.T) {
+	parser := New()
+	data := `{"driver": "postgres", "host": "localhost", "database": "postgres", "user": "user", "password": "123"}`
+	b := bytes.NewBufferString(data)
+	dbConf, err := parser.ParseDBConf(b)
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+
+	if dbConf.Host != "localhost" {
+		t.Errorf("host was not parsed correctly, expected %s, got %s", "localhost", dbConf.Host)
+	}
+	if dbConf.Driver != "postgres" {
+		t.Errorf("driver was not parsed correctly, expected %s, got %s", "postgres", dbConf.Host)
+	}
+	if dbConf.Database != "postgres" {
+		t.Errorf("database was not parsed correctly, expected %s, got %s", "postgres", dbConf.Host)
+	}
+	if dbConf.User != "user" {
+		t.Errorf("user was not parsed correctly, expected %s, got %s", "user", dbConf.Host)
+	}
+	if dbConf.Password != "123" {
+		t.Errorf("password was not parsed correctly, expected %s, got %s", "123", dbConf.Host)
+	}
+}
