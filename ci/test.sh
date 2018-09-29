acf1059e291dd43c7392f6b14be214015b0dbb5c
Islam Shehata <i.shehata@gastrofix.com>
2018-09-29 17:28:57 +0200

    add yaml configuration for concourse

---
+++ ci/test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "go path is" $GOPATH
+echo $(pwd)
+echo $(ls code)
+p=$GOPATH/src/github.com/ishehata/gofixtures
+mkdir -p $p
+cp -R code/* $p/
+cd $p
+echo "verify that code files are copied"
+echo "pwd is $(pwd)"
+echo $(ls)
+echo "prepairing to start go test"
+go test -v .
