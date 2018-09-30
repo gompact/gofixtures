b20807c5b52a9e8fdb60d14e9a4b87f96afbeecc
Islam Shehata <i.shehata@gastrofix.com>
2018-09-30 20:36:43 +0200

    fixing test task

--- ci/lint.sh
+++ ci/lint.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
-echo "go path is" $GOPATH
-echo $(pwd)
-echo $(ls code)
 p=$GOPATH/src/github.com/ishehata/gofixtures
 mkdir -p $p
 cp -R code/* $p/
 cd $p
-echo "verify that code files are copied"
-echo "pwd is $(pwd)"
-echo $(ls)
-echo "installing dependencies"
-go get
-echo "prepairing to start go test"
+echo "calling go vet..."
 go vet .
